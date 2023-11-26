49e600792f2dd39002d958d31574941e06b80bf1
Michael <thesuspect182@gmail.com>
2023-11-26 16:51:21 +0300

    created models for entities

--- AliStore/AliStore_tz/models.py
+++ AliStore/AliStore_tz/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    # "on_delete=models.CASCADE" - action if an object is deleted. If a product category is deleted, all products from
+    # this category will also be deleted
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    manufacturer = models.ForeignKey('Manufacturer', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    parent_category = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Manufacturer(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+    
