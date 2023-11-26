0101bfc785331de8072a2e34b7dad804277395a9
Michael <thesuspect182@gmail.com>
2023-11-26 19:16:58 +0300

    created model for characteristics

--- AliStore/AliStore_tz/models.py
+++ AliStore/AliStore_tz/models.py
@@ -28,3 +28,20 @@ class Manufacturer(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Characteristic(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    PRIMARY = 'primary'
+    SECONDARY = 'secondary'
+    TYPE_CHOICES = [
+        (PRIMARY, 'Primary'),
+        (SECONDARY, 'Secondary'),
+    ]
+    type = models.CharField(max_length=20, choices=TYPE_CHOICES)
+    values = models.TextField()
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='characteristics')
+
+    def __str__(self):
+        return self.name
