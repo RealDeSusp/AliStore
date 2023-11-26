485fbfe64237f9e341eaa2114078cd69c430e445
Michael <thesuspect182@gmail.com>
2023-11-27 01:50:03 +0300

    changed logic in models.py

--- AliStore/AliStore_tz/models.py
+++ AliStore/AliStore_tz/models.py
@@ -2,53 +2,46 @@ from django.db import models
 
 
 class Product(models.Model):
-    id = models.AutoField(primary_key=True)
+    identifier = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
-    # "on_delete=models.CASCADE" - action if an object is deleted. If a product category is deleted, all products from
-    # this category will also be deleted
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
     manufacturer = models.ForeignKey('Manufacturer', on_delete=models.CASCADE)
-    characteristic = models.ManyToManyField('Characteristic')
 
-    def __str__(self):
-        return self.name
+    def product_characteristics(self):
+        # Get all characteristics for the given category
+        category_characteristics = self.category.characteristics.all()
+
+        # Get all characteristics for the product through the intermediate table
+        product_characteristics = self.productcharacteristics_set.all()
+
+        # Return the union of category and product characteristics
+        return category_characteristics | product_characteristics
 
 
 class Category(models.Model):
-    id = models.AutoField(primary_key=True)
+    identifier = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
-    parentcategory = models.ForeignKey('ParentCategory', on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
+    parent_category = models.ForeignKey('ParentCategory', on_delete=models.CASCADE, null=True, blank=True)
 
 
 class ParentCategory(models.Model):
-    id = models.AutoField(primary_key=True)
+    identifier = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.name
 
 
 class Manufacturer(models.Model):
-    id = models.AutoField(primary_key=True)
+    identifier = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.name
 
 
 class Characteristic(models.Model):
-    id = models.AutoField(primary_key=True)
+    identifier = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
-    PRIMARY = 'primary'
-    SECONDARY = 'secondary'
-    TYPE_CHOICES = [
-        (PRIMARY, 'Primary'),
-        (SECONDARY, 'Secondary'),
-    ]
-    type = models.CharField(max_length=20, choices=TYPE_CHOICES)
+    type = models.CharField(max_length=255)
+    values = models.JSONField()
 
-    def __str__(self):
-        return self.name
+
+class ProductCharacteristics(models.Model):
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    characteristic = models.ForeignKey('Characteristic', on_delete=models.CASCADE)
+    value = models.CharField(max_length=255)
